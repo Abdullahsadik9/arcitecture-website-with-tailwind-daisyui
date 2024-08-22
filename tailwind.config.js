f58e0fdab817537b8a42346d94daca79989b198a
Abdullahsadik9 <abdullahsadik9@gmail.com>
2024-08-22 14:20:18 +0600

    next project sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('img/bg.jpg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
   },
   plugins: [],
 };
